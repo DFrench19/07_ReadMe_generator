bfe4bd4b78100f7048132ea8e146bc9662eb48a0
David French <David@mefalf.net>
2020-11-06 15:33:54 -0600

    adding files to github

---
+++ index.js
@@ -0,0 +1,62 @@
+// array of questions for user
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your project name.',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project.',
+        name: 'desctiption',
+    },
+    {
+        type: 'input',
+        message: 'What kind of license should your project have?',
+        name: 'license',
+    },
+
+
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'command',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'command',
+    },
+    {
+        type: 'input',
+        message: 'What doses the user need to know about using the repo?',
+        name: 'info',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contirbuting to the repo?',
+        name: 'desctiption',
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
