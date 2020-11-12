2147a0babfc0a74e333ee55e443e62954b0876ec
David French <David@mefalf.net>
2020-11-11 22:54:54 -0600

    deleting html file and modified js file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
 
 
 
 // array of questions for user
-inquirer
-    .prompt([
+const questions =
+([
         {
             type: 'input',
             message: 'What is your GitHub username?',
@@ -55,34 +56,31 @@ inquirer
             name: 'contirbuting',
         },
     ])
-    .then(({ name, email, description, github, license, info, contirbuting, test, command }) => {
-        const html =
-
-            `<!DOCTYPE html>
-    <html lang=“en-us”>
-    <body>
-        <h1> ${name}</h1>
-        <p> ${github}</p>
-        <p> ${email}</p>
-        <p> ${description}</p>
-        <p> ${license}</p>
-        <p> ${info}</p>
-        <p> ${contirbuting}</p>
-        <p> ${test}</p>
-        <p> ${command}</p>
-    </body>
-</html>`;
 
         // function to write README file
-        fs.writeFile('index.html', html, err => err ? console.error(err) :
-            console.log("Generating ReadME!"));
+       function writeToFile(fileName, date) {
+           fs.writeFile(fileName, html, err => {
+               if (err) {
+                   return console.log(err);
+               }
+               console.log("Generating ReadMe....")
+           })
+       }
+       const writeFileAsync = util.promisify(writeToFile);
 
         // // function to initialize program
-        function init() {
+     async function init() {
+            try {
+                const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
 
+            }
+            finally {
+
+            }
         }
 
         // // function call to initialize program
         init();
 
-    })
+  
